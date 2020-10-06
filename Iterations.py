81114de8f1e3525e4a4f8e8bbb7c770f403d96ad
Imac <dio351a@gmail.com>
2020-10-06 13:02:26 -0500

    Iterations

--- Iterations.py
+++ Iterations.py
@@ -1,5 +1,20 @@
-counter = 0
+# counter = 0
 
-while counter < 10:
-    print(counter)
-    counter += 1
+# while counter < 10:
+#     print(counter)
+#     counter += 1
+
+## Clock Like Counter
+external_counter = 0
+internal_counter = 0
+
+while external_counter < 12:
+    while internal_counter < 60:
+        print(external_counter, internal_counter)
+        internal_counter += 1
+
+        if internal_counter >= 30:
+            break
+    external_counter += 1
+    internal_counter = 0   
+
